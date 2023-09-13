1ed976afe4321c6649a29f49a8c2a9b726b617b0
MediumWaffle <aaronjarnes@gmail.com>
2023-09-12 17:59:28 -0700

    fixed almost all errors, seg fault when printing. The tokens are not being copied into the correct file locations

--- pa1.c
+++ pa1.c
@@ -20,39 +20,61 @@
 int getTokens(char *s, char ***args){
     int numOfTokens=0;
 
+    //allocate enough memory for length of string
+    printf("Space Allocating: %ld\n", ((strlen(s)+1)/2));
+    //*args=malloc((((strlen(s)+1)/2)+1) *sizeof(char *));
+    char * array[20];
+    *args=array;
+
     //if theres nothing in the token return -1
     if (s[0] == '\0'){return -1;}
     //if the beginning of the string is a space, loop until no space and move both pointers to first token.
     int i=0; //so it can be used outside of the scope. 
 
     //check if whitespace before any tokens, if so ++i to ignore them
-    for(i=i; i<strlen(s); ++i){
+    for(i=0; i<strlen(s); ++i){
         if(s[i] == ' '){
             ++i;
+        } else {
+            break;
         }
     }
 
     //first token, read til end of word, count along the way, then read it into a char*
-    char* word=""; //tokens we build along the way
+    char word[100]; //tokens we build along the way
     int k=0; //loop through args
-    for(i=i; i<(strlen(s)-1); ++i){
-        if(s[i]!=' ' || s[i]!='\0'){
+    printf("Number of Beginning White Spaces: %d\n",i);
+    for(i=i; i<strlen(s)+1; ++i){
+        printf("Character Read In: %c\n",s[i]);
+        if(s[i]!=' ' && s[i]!='\0'){
             strncat(word, &s[i], 1); //strncat appends a null terminating character so i dont have to
+            printf("CHAR word[]:%s\n", word);
         } else {
             if(s[i-1]!=' '){ //if the previous char wasnt a whitespace, then put the word in args
-                (*args)[k]=malloc(strlen(word)); //Put word in token array
-                strncpy((*args)[k], word, strlen(word));
+                printf("Adding token to args\n");
+                (*args)[k]=malloc((strlen(word)+1) * sizeof(char)); //Put word in token array
+                strncpy((*args)[k], word, strlen(word)+1);
                 ++k;
                 strcpy(word, ""); //clear word
+                printf("%s\n",word);
                 ++numOfTokens;
             }
         }
+        printf("\n");
     }
     //Done with main loop, add null to the end of the array
-    char* nullptr='\0';
     ++numOfTokens;
-    (*args)[k]=malloc(1); //Put word in token array
-    strncpy((*args)[k], nullptr, 1);
+    (*args)[k]=NULL;
+
+
+     if(numOfTokens == -1){
+        printf("Nothing Was Entered\n");
+    }
+    printf("Number of Tokens: %d\n", (numOfTokens-1));
+    for(int i=0; i<numOfTokens; ++i){
+        printf("Token %d: %s\n", i, (*args)[i]);
+    }
+
     return numOfTokens;
 }
 
@@ -65,8 +87,10 @@ int getTokens(char *s, char ***args){
  *        number of tokens in total. 
 */
 void printTokens(char*** args, int n){
-    if(n == -1){printf("Nothing Was Entered");}
-    printf("Number of Tokens: %d", (n-1));
+    if(n == -1){
+        printf("Nothing Was Entered\n");
+    }
+    printf("Number of Tokens: %d\n", (n-1));
     for(int i=0; i<n; ++i){
         printf("Token %d: %s\n", i, *args[i]);
     }
